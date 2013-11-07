d7b26adaa745486beb24ca88e0563de2f64bd606
LiuHui <liuhui-751@163.com>
2013-11-07 23:26:38 +0800

    UPDATE DATABASE CREATE SQL

--- db.sql
+++ db.sql
@@ -1,6 +1,5 @@
-CREATE DATABASE IF NOT EXISTS enviroment_spider DEFAULT CHARACTER SET 'utf8';
-
-USE enviroment_spider
+CREATE DATABASE IF NOT EXISTS spider DEFAULT CHARACTER SET 'utf8';
+USE spider
 
 CREATE TABLE IF NOT EXISTS airq (
                                     id              integer,
@@ -13,4 +12,20 @@ CREATE TABLE IF NOT EXISTS airq (
                                 );
 
 GRANT ALL PRIVILEGES ON enviroment_spider.* TO 'spider'@'localhost' IDENTIFIED BY 'spider';
+FLUSH PRIVILEGES;
+
+CREATE TABLE IF NOT EXISTS ifengHeadline (
+                                            id      INT AUTO_INCREMENT,  -- 编号
+                                            title   CHAR(255)   NOT NULL, -- 新闻标题
+                                            href    CHAR(255)   NOT NULL,   -- 相应的连接
+                                            uptime  DATETIME,  -- 插入时间，非网站发布时间
+                                            pri     TINYINT(1)  UNSIGNED,   -- 优先级,重要性
+                                            content TEXT    DEFAULT NULL,   -- 报道内容
+                                            src     CHAR(25)    DEFAULT NULL,   -- 来源
+                                            editor  CHAR(25)    DEFAULT NULL,   -- 编辑
+                                            tags    CHAR(255)   DEFAULT NULL,   -- TAG
+                                            PRIMARY KEY (id, title)
+                                            );
+
+GRANT ALL PRIVILEGES ON spider.* TO 'spider'@'localhost' IDENTIFIED BY 'spider';
 FLUSH PRIVILEGES;
